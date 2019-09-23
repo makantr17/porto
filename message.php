d522ecaa8c3f71822959e6b81b91ed06b78dea21
Mamady <43143917+makantr17@users.noreply.github.com>
2019-09-23 19:32:06 +0200

    Update message.php

--- message.php
+++ message.php
@@ -6,7 +6,7 @@
    $date1 = date("Y-m-d H:i:s");
 
 // compare email and password from the data base
-$db = mysqli_connect("localhost", "root", "", "portofolio");
+$db = mysqli_connect("db4free.net", "mamadykante", "123456789", "portofolio");
 if (mysqli_connect_errno()){
     echo 'Failed to connect to MYSQLI'. mysqli_connect_errno();
 }
