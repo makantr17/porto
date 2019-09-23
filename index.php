c75db9f122b5dc55938224f8b60da80b8f2c0a62
Mamady <43143917+makantr17@users.noreply.github.com>
2019-09-23 19:32:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   include 'header.php';
 
-  $db = mysqli_connect("localhost", "root", "", "portofolio");
+  $db = mysqli_connect("db4free.net", "mamadykante", "123456789", "portofolio");
 if (mysqli_connect_errno()){
     echo 'Failed to connect to MYSQLI'. mysqli_connect_errno();
 }
